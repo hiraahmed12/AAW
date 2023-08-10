4d57cd2460412428b346b2ec265ba50d3f0f75c0
hiraahmed12 <136683709+hiraahmed12@users.noreply.github.com>
2023-08-10 10:34:07 +0500

    Add files via upload

---
+++ AAW/leo.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >=10) {
+        $(".navbar").addClass("nav-scroll");
+        $(".back_to_top").addClass("show_btn");
+    } else {
+        $(".navbar").removeClass("nav-scroll")
+        $(".back_to_top").removeClass("show_btn")
+    }
+})
+
+AOS.init();
